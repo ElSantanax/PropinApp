6b2f2d1bad53581d2387719e59709990c8b0c34a
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-10 03:43:57 -0500

    PropinApp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-black text-cyan-50"> Calculadora de propina y consumos</h1>
+      </header>
+
+    </>
+  )
+}
+
+export default App
