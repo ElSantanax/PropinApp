013c5a2863f7c5ba32109db8cae07a8f5cdc9d16
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-10 03:55:32 -0500

    Agregar tipo MenuItem y datos del menú a la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
       <header className=" bg-teal-400 py-5">
         <h1 className=" text-center text-4xl font-black text-cyan-50"> Calculadora de propina y consumos</h1>
       </header>
-
     </>
   )
 }
