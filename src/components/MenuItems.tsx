696fdd07440cd72f1bc2df8e276f1f946b9ce3b1
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-11 13:39:11 -0500

    Actualizar la función addItem en useOrder para aceptar un MenuItem y modificar el componente MenuItems para pasar el item al agregarlo

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -2,14 +2,14 @@ import { MenuItem } from "../types"
 
 type MenuItemsPromps = {
     item: MenuItem,
-    addItem: () => void
+    addItem: (item: MenuItem) => void
 }
 
 export default function MenuItems({ item, addItem }: MenuItemsPromps) {
     return (
         <button
             className="border border-teal-400 hover:bg-teal-400 hover:text-white rounded-lg w-full p-3 flex justify-between"
-            onClick={() => addItem()}
+            onClick={() => addItem(item)}
         >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
